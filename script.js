1158edbef8a1f2d8939f90e947a5922724c3332a
CYC0630 <tsai.yuchen0630@gmail.com>
2023-09-15 08:55:12 +0800

    空殼
    雛形

---
+++ script.js
@@ -0,0 +1,17 @@
+const ROWS = 3; //橫列
+const COLUMNS = 3; //直行
+
+const game = document.getElementById("game"); //主棋盤
+const table = new Array(ROWS);
+for (let row = 0; row < ROWS; row++)
+{
+    table[row] = new Array(COLUMNS);
+    for (let column = 0; column < COLUMNS; column++)
+    {
+        
+    }
+}
+
+function clickLight(mouseEvent)
+{
+}
